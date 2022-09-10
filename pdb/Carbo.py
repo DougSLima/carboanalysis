e68b5e1eeb4860a519aabeb6137d820f01dca5f3
Douglas <douglassilvadelima5@gmail.com>
2022-09-10 00:51:57 -0300

    Lógica dos filtros transferidos para modelo de funções

--- pdb/Carbo.py
+++ pdb/Carbo.py
@@ -1,28 +1,52 @@
 import os
+import struct
 from Bio.PDB import *
+import warnings
+from Bio.PDB.PDBExceptions import PDBConstructionWarning #ignorar warning (PDBConstructionWarning: WARNING: Chain B is discontinuous at line numeroDaLinha.)
 
-pdbParser = PDBParser()
+warnings.simplefilter('ignore', PDBConstructionWarning)
+
+# Remove da lista recebida todos os nomes de arquivos cujas estruturas apresentem resoluções maiores que a resolução máxima selecionada
+def filter_maxResolution(fileNames, maxResolution):
+
+    filteredFileNames = [] # lista de filtrados
+    pdbParser = PDBParser()
+
+    for fileName in fileNames:
+        file = open(fileName, 'r')
+        structure = pdbParser.get_structure(file.name, file)
+        
+        if structure.header.get("resolution") <= maxResolution: # Testa se a resolução da estrutura é menor ou igual a resolução desejada (atributo da função)
+            filteredFileNames.append(fileName)
+    
+    return filteredFileNames
+
+# Remove da lista recebida todos os nomes de arquivos cujas estruturas apresentem OWAB maiores que o OWAB máximo selecionado
+def filter_maxOWAB(fileNames, maxOWAB):
+
+    filteredFileNames = []
+    pdbParser = PDBParser()
+
+    for fileName in fileNames:
+        file = open(fileName, 'r')
+        structure = pdbParser.get_structure(file.name, file)
+        atomList = Selection.unfold_entities(structure, 'A')
+        occupancy_x_bfactor = 0
+
+        for atom in atomList:
+            occupancy_x_bfactor += (atom.get_occupancy() * atom.get_bfactor())
+        
+        owab = occupancy_x_bfactor/len(atomList) # OWAB = média de ocupância * b_factor dos átomos da estrutura
+        print(fileName)
+        print(owab)
+        if owab <= maxOWAB:
+            filteredFileNames.append(fileName)
+    
+    return filteredFileNames
+
 
 os.chdir("/home/douglas_lima/pdb/testes")
 fileNames = os.listdir("/home/douglas_lima/pdb/testes")
-
-for fileName in fileNames:
-    file = open(fileName, 'r')
-    structure = pdbParser.get_structure(file.name, file)
-    print(structure.header.get("resolution"))
-
-# for fileName in fileNames:
-#     file = open(fileName, 'r')
-#     structure = pdbParser.get_structure(file.name, file)
-#     atomList = Selection.unfold_entities(structure, 'A')
-#     occupancy_x_bfactor = 0
-
-#     for atom in atomList:
-#         occupancy_x_bfactor += (atom.get_occupancy() * atom.get_bfactor())
-    
-#     owab = occupancy_x_bfactor/len(atomList)
-#     print("Nome: " + str(fileName) + " - OWAB: " + str(owab))
-
 
 
 
